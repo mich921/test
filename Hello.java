37f9f96978a228489253a2480b084523025e133b
mich921 <113414830+mich921@users.noreply.github.com>
2022-09-17 00:37:58 +0300

    refactoring file

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 	      System.out.println("AAAA");
-        System.out.println("Hello, World!12");
+       	      System.out.println("Hello, World!12");
 	      System.out.println("BBBB");
     }
 }
